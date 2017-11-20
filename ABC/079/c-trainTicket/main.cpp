56aa095f58206ef730124639bf25f2f75bf9aafa
SRkuma <nuru1709oath@gmail.com>
2017-11-21 08:33:27 +0900

    C問題とD問題の解答

--- ABC/079/c-trainTicket/main.cpp
+++ ABC/079/c-trainTicket/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <math.h>
 typedef long long ll;
 
 #define FOR(i,a,b) for(ll i = (a);i < (b);++i)
@@ -10,6 +10,31 @@ typedef long long ll;
 using namespace std;
 
 int main(void){
-    
+
+  string str;
+  cin >> str;
+  
+  int a,b,c,d;
+  a = str[0] - '0';
+  b = str[1] - '0';
+  c = str[2] - '0';
+  d = str[3] - '0';
+  REP(k,2){
+    int op1 = pow(-1,k);
+    REP(i,2){
+    int op2 = pow(-1,i);
+      REP(j,2){
+      int op3 = pow(-1,j);
+        if(a+op1*b+op2*c+op3*d == 7){
+          cout << a << (op1 == 1 ? '+' : '-') << 
+                  b << (op2 == 1 ? '+' : '-') <<
+                  c << (op3 == 1 ? '+' : '-') <<
+                  d << "=7" << endl;
+          return 0;
+        }
+      }
+    }
+  }
+
   return 0;
 }
--- ABC/079/d-wall/main.cpp
+++ ABC/079/d-wall/main.cpp
@@ -9,7 +9,53 @@ typedef long long ll;
 
 using namespace std;
 
+#define N 10
+
+int cost[N][N];
+
+void warshall_floyd(){
+  REP(k,N){
+      REP(i,N){
+          REP(j,N){
+            cost[i][j] = min(cost[i][j], cost[i][k] + cost[k][j]);
+          }
+      }
+  }
+}
+
 int main(void){
+
+  int h,w;
+  cin >> h >> w;
+
+  REP(i,N){
+    REP(k,N){
+      cin >> cost[i][k];
+    }
+  }
+
+  warshall_floyd();
+
+  int a[h][w];
+
+  REP(i,h){
+    REP(k,w){
+      cin >> a[i][k];
+    }
+  }
+
+  long long ans = 0;
+
+  REP(i,h){
+    REP(k,w){
+
+      if(a[i][k] == -1 || a[i][k] == 1) continue;
+      
+      ans += cost[a[i][k]][1];
+    }
+  }
+
+  cout << ans << endl;
     
   return 0;
 }
